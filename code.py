33482174cfa3f189bb22a923dd3adaff3ddf2008
Marpaka Pradeep Sai <153713502+MarpakaPradeepSai@users.noreply.github.com>
2024-09-28 14:29:42 +0530

    Update code.py

--- code.py
+++ code.py
@@ -19,7 +19,7 @@ with col1:
 
 with col2:
     AGE = st.number_input("AGE (owner-occupied units built before 1940)", min_value=0.0)
-    DIS = st.number_input("DIS (weighted distances to five Boston employment centers)", min_value=0.0)
+    DIS = st.number_input("DIS (distances to Boston employment centers)", min_value=0.0)
     TAX = st.number_input("TAX (full-value property tax rate per $10,000)", min_value=0)
 
 # Create a new row for the next set of inputs
